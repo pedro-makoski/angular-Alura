d50f1c71d71898003ae49ac5859e1590c41823dc
pedro-makoski <148820090+pedro-makoski@users.noreply.github.com>
2025-02-14 19:34:30 -0300

    Melhorias

--- JS/Curso-2-fokus/Fokus-projeto-base/script.js
+++ JS/Curso-2-fokus/Fokus-projeto-base/script.js
@@ -3,25 +3,28 @@ const focoButton = document.querySelector(".app__card-button--foco")
 const descansoCurtoButton = document.querySelector(".app__card-button--curto")
 const descansoLongoButton = document.querySelector(".app__card-button--longo")
 
-const ATRIBUTE_INFO_STATUS = "data-contexto"
-
-focoButton.addEventListener("click", () => {
-    html.setAttribute(ATRIBUTE_INFO_STATUS, "foco")
-})
-
-descansoCurtoButton.addEventListener("click", () => {
-    html.setAttribute(ATRIBUTE_INFO_STATUS, "descanso-curto")
-})
-
-descansoLongoButton.addEventListener("click", () => {
-    html.setAttribute(ATRIBUTE_INFO_STATUS, "descanso-longo")
-})
-
 const timer = document.getElementById("timer")
-const appImage = document.querySelector(".app-image")
+const appImage = document.querySelector(".app__image")
 const appTittle = document.querySelector(".app__title")
 const initTimer = document.getElementById("start-pause")
 
 const MAX_FOCUS = 1500
 const CURTO_DESCANSO = 300
 const LONGO_DESCANSO = 900
+
+const ATRIBUTE_INFO_STATUS = "data-contexto"
+
+focoButton.addEventListener("click", () => {
+    html.setAttribute(ATRIBUTE_INFO_STATUS, "foco");
+    appImage.setAttribute('src', './imagens/foco.png')
+})
+
+descansoCurtoButton.addEventListener("click", () => {
+    html.setAttribute(ATRIBUTE_INFO_STATUS, "descanso-curto")
+    appImage.setAttribute('src', './imagens/descanso-curto.png')
+})
+
+descansoLongoButton.addEventListener("click", () => {
+    html.setAttribute(ATRIBUTE_INFO_STATUS, "descanso-longo")
+    appImage.setAttribute('src', './imagens/descanso-longo.png')
+})
