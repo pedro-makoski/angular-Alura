c3682da807c0fd8d17f11c1668b3d7b72842bb79
pedro-makoski <148820090+pedro-makoski@users.noreply.github.com>
2025-02-18 19:14:02 -0300

    Finalização do projeto fokus

--- JS/Curso-2-fokus/Fokus-projeto-base/script.js
+++ JS/Curso-2-fokus/Fokus-projeto-base/script.js
@@ -8,17 +8,20 @@ const appImage = document.querySelector(".app__image");
 const appTittle = document.querySelector(".app__title");
 const initTimer = document.getElementById("start-pause");
 const botaoPlay = document.getElementById("start-pause");
+const iniciarOuPausarButton = document.querySelector("#start-pause span");
+const iniciarOuPausarButtonImg = document.querySelector("#start-pause img");
+const tempoNaTela = document.getElementById("timer");
 
 const allButtons = document.querySelectorAll(".app__card-button");
 
 const musicaFocoInput = document.getElementById("alternar-musica");
 const musicaFocus = new Audio('./sons/luna-rise-part-one.mp3');
-const playAudio = new Audio("./sons/play.wav    ");
+const playAudio = new Audio("./sons/play.wav");
 const pauseAudio = new Audio("./sons/pause.mp3");
 const acabouTempo = new Audio("./sons/beep.mp3");
 musicaFocus.loop = true;
 
-let tempoDecorridoEmSegundos = 5;
+let tempoDecorridoEmSegundos = 1500;
 let intervaloId = null; 
 
 musicaFocoInput.addEventListener("change", () => {
@@ -60,17 +63,22 @@ function alterarContexto(contexto) {
 
     switch(contexto) {
         case "foco":
+            tempoDecorridoEmSegundos = 1500;
             appTittle.innerHTML = `Otimize sua produtividade,<br><strong class="app__title-strong">mergulhe no que importa.</strong>`;
             break;
         case "descanso-curto":
+            tempoDecorridoEmSegundos = 300;
             appTittle.innerHTML = `Que tal dar uma respirada?<br><strong class="app__title-strong">Faça uma pausa curta</strong>`
             break;
         case "descanso-longo":
+            tempoDecorridoEmSegundos = 900;
             appTittle.innerHTML = `Hora de voltar à superfície<br><strong class="app__title-strong">Faça uma pausa longa</strong>`
             break;
         default:
             break;
     }
+
+    mostrarTempo();
 }
 
 const contagemRegressiva = () => {
@@ -81,23 +89,36 @@ const contagemRegressiva = () => {
         pararContador();
         return 
     }
-    tempoDecorridoEmSegundos-=1 
-    console.log("Tempo: "+tempoDecorridoEmSegundos)
+    tempoDecorridoEmSegundos-=1;
+    mostrarTempo()
 }
 
 botaoPlay.addEventListener("click", iniciarOuPausar)
 
 function iniciarOuPausar() {
     if(intervaloId) {
-        pauseAudio.play()
+        pauseAudio.play();
         pararContador();
         return;
     }
     playAudio.play();
+    iniciarOuPausarButtonImg.setAttribute("src", "./imagens/pause.png");
+    iniciarOuPausarButton.textContent = "pausar";
+
     intervaloId = setInterval(contagemRegressiva, 1000);
 }
 
 function pararContador() {
+    iniciarOuPausarButton.textContent = "Começar";
+    iniciarOuPausarButtonImg.setAttribute("src", "./imagens/play_arrow.png");
     clearInterval(intervaloId);
     intervaloId = null;
 }
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleString('pt-br', {minute: "2-digit", second:"2-digit"});
+    tempoNaTela.textContent = tempoFormatado;
+}
+
+mostrarTempo()
